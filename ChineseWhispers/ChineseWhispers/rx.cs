0a6d7b4f429bc4541f5e506d24c4c52511ee37ee
stavlut <stavlut@gmail.com>
2017-01-01 11:35:59 +0200

    commit

--- ChineseWhispers/ChineseWhispers/rx.cs
+++ ChineseWhispers/ChineseWhispers/rx.cs
@@ -6,27 +6,30 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ChineseWhispers
 {
     class rx
     {
+       static byte[] Buffer { get; set; }
         bool on;
-        TcpListener tcp;
-        UdpClient udp;
+        Socket tcpListener;
+        Socket udp;
+        IPEndPoint ipLocal;
         public rx()
         {
             on = false;
-            udp = new UdpClient(6000);
-            IPHostEntry host;            
-            host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (IPAddress ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
+            udp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Udp);
+            ipLocal = (IPEndPoint)tcpListener.LocalEndPoint;
+
+
                     for (int i = 6001; i < 7000; i++)
                     {
                         try {
-                            tcp = new TcpListener(ip, i);
+                            tcpListener = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+                            tcpListener.Bind(new IPEndPoint(ipLocal.Address, i));
+                            udp.Bind(new IPEndPoint(ipLocal.Address, 6000));
+
                             break;
                         } 
                         catch
@@ -34,11 +37,53 @@ namespace ChineseWhispers
                             continue;
                         }
                     }                   
-                }
-            }
+                       
+            tcpListener.Listen(1);
+            udp.Listen(1);
+        }
+        public void sendOffer()
+        {
+            Socket accepted = udp.Accept();
             
+            Buffer = new byte[accepted.SendBufferSize];
+            int bytesRead = accepted.Receive(Buffer);
+            byte[] formatted = new byte[bytesRead];
+            for (int i = 0; i < bytesRead; i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            string strData = Encoding.ASCII.GetString(formatted);
+            EndPoint cameFrom= accepted.RemoteEndPoint;
+            byte[] msg = Encoding.ASCII.GetBytes(strData + ipLocal.Address + ((IPEndPoint)(tcpListener.LocalEndPoint)).Port.ToString());
+            udp.SendTo(msg, cameFrom);
+        }
+        public string TcpReciveConnection()
+        {
+            Socket accepted = tcpListener.Accept();
+            udp.Close();
+            on = true;
+            Buffer = new byte[accepted.SendBufferSize];
+            int bytesRead = accepted.Receive(Buffer);
+            byte[] formatted = new byte[bytesRead];
+            for (int i = 0; i < bytesRead; i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            string strData = Encoding.ASCII.GetString(formatted);
+            ///////remember to add change randomly
+            Random rnd = new Random();
+            int place = rnd.Next(1, strData.Length-1);
+            int letterrandom = rnd.Next(0, 26); // Zero to 25
+            char insertRandomChar = (char)letterrandom;
+            var aStringBuilder = new StringBuilder(strData);
+            aStringBuilder.Remove(place, 1);
+            aStringBuilder.Insert(place, insertRandomChar);
+            strData = aStringBuilder.ToString();
+            return strData;
         }
 
-        
+
+
+
     }
 }
