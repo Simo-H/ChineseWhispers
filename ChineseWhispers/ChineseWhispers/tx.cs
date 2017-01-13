e0cdfb3699e6ae1ffa00c96ecb372489be8f1ef4
Simo <Simo@SIMO>
2017-01-13 13:18:35 +0200

    a

--- ChineseWhispers/ChineseWhispers/tx.cs
+++ ChineseWhispers/ChineseWhispers/tx.cs
@@ -104,10 +104,10 @@ namespace ChineseWhispers
             {
                 //m.WaitOne();
                 tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                if (rx.connectedIp != null && rx.connectedIp.ToString().Equals(((IPEndPoint)remoteEndPoint).Address.ToString()))
-                {
-                    return;
-                }
+                //if (rx.connectedIp != null && rx.connectedIp.ToString().Equals(((IPEndPoint)remoteEndPoint).Address.ToString()))
+                //{
+                //    return;
+                //}
                 //tcpClient.Connect(remoteEndPoint);
                 rx.connectedIp = ((IPEndPoint)(remoteEndPoint)).Address;
                 Console.WriteLine("IP: " + rx.GetLocalIPAddress().ToString() + " Port: " + ((IPEndPoint)(udp.LocalEndPoint)).Port.ToString() + " Connected succesfully via TCP to IP: " + ((IPEndPoint)(remoteEndPoint)).Address + " Port " + ((IPEndPoint)(remoteEndPoint)).Port);
