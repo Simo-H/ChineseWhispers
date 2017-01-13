24e96ed35476d757cd1958e5e8e18fae0c1adc4e
Simo <Simo@SIMO>
2017-01-13 12:45:51 +0200

    a

--- ChineseWhispers/ChineseWhispers/rx.cs
+++ ChineseWhispers/ChineseWhispers/rx.cs
@@ -128,10 +128,10 @@ namespace ChineseWhispers
                 {
                     //tx.m.WaitOne();
                     accepted = tcpListener.Accept();
-                    if (rx.connectedIp != null && rx.connectedIp.ToString().Equals(((IPEndPoint)accepted.RemoteEndPoint).Address.ToString()))
-                    {
-                        return;
-                    }
+                    //if (rx.connectedIp != null && rx.connectedIp.ToString().Equals(((IPEndPoint)accepted.RemoteEndPoint).Address.ToString()))
+                    //{
+                    //    return;
+                    //}
                     connectedIp = ((IPEndPoint)accepted.RemoteEndPoint).Address;
                     //tx.m.ReleaseMutex();
                     CWsystem.writer.WriteToLog("IP:" + rx.GetLocalIPAddress().ToString() + " Port: " + ((IPEndPoint)(udp.LocalEndPoint)).Port.ToString() + " accepted TCP Connection From IP: " + connectedIp + " Port " + ((IPEndPoint)accepted.RemoteEndPoint).Port);
